be0444d25cb883ef4628b632b7ebdf1a476dfb95
MelatiGithub23 <melati23.github@gmail.com>
2019-09-24 13:51:14 +0800

    tugas2

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login', 'HomeController@myCaptcha')->name('myCaptcha');
+Route::post('login', 'HomeController@myCaptchaPost')->name('myCaptcha.post');
+Route::get('refresh_captcha', 'HomeController@refresh_captcha')->name('refresh_captcha');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
